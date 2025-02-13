8c2b009c8b55b9052468cf784313ffc2de8b336c
David Buitrago <david.buitrgo@qualitasfunds.com>
2025-02-13 09:11:13 +0100

    Refactor HTML and CSS, update login page design

--- src/Pages/Login.tsx
+++ src/Pages/Login.tsx
@@ -1,66 +1,136 @@
-import React, {useState} from 'react';
-import supabase from '../helper/supabaseClient';
-import { Link, useNavigate } from 'react-router';
+import React, { useState } from "react";
+import supabase from "../helper/supabaseClient";
+import { Link, useNavigate } from "react-router";
 
-import orbe from '../assets/orbe.gif'
-import blueOrbe from '../assets/blueOrbe.gif'
+import orbe from "../assets/orbe.gif";
 
 function Login() {
   const navigate = useNavigate();
-  const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [message, setMessage] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [message, setMessage] = useState("");
 
-    const handleSubmit = async (e: React.FormEvent) => {
-        e.preventDefault();
-       setMessage('');
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    setMessage("");
 
-       const {data, error} = await supabase.auth.signInWithPassword({
-        email: email,
-        password: password
-       })
+    const { data, error } = await supabase.auth.signInWithPassword({
+      email: email,
+      password: password,
+    });
 
-         if (error) {
-          setMessage(error.message);
-          setEmail('');
-          setPassword('');
-          return;
-         }
+    if (error) {
+      setMessage(error.message);
+      setEmail("");
+      setPassword("");
+      return;
+    }
 
-         if (data) {
-          navigate('/dashboard');
-          return null
-         }
-    };
+    if (data) {
+      navigate("/dashboard");
+      return null;
+    }
+  };
 
   return (
     <div>
-      <h1>Login</h1>
-    <div>    
-    </div>
+      {/*<h1>Login</h1>
+      <div></div>
 
-    <div className='login-container'>
-    <img src={orbe} alt="Vite logo" className="logo"/>
-    <img src={blueOrbe} alt="Vite logo" className="logo react"/>
-    <form onSubmit={handleSubmit}>
-            <input
-                type="email"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                placeholder="Email"
-            />
-            <input
-                type="password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                placeholder="Password"
-            />
-            <button type="submit">Log in</button>
-            <p>{message}</p>
+       <div className="login-container">
+        <img src={blueOrbe} alt="Vite logo" className="logo react" />
+        <form onSubmit={handleSubmit}>
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="Email"
+            required
+            className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+          />
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            placeholder="Password"
+          />
+          <button type="submit">Log in</button>
+          <p>{message}</p>
         </form>
         <span>Don't have an account? </span>
         <Link to="/Register">Register</Link>
-    </div>
+      </div> */}
+      <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8 login-container">
+        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+          <img className="mx-auto h-20 w-auto" src={orbe} alt="Your Company" />
+          <h2 className="text-center text-2xl/9 font-bold tracking-tight text-white-900">
+            Sign in to your account
+          </h2>
+        </div>
+
+        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+          <form className="space-y-6" onSubmit={handleSubmit}>
+            <div>
+              <label className="block text-sm/6 font-medium text-white-900">
+                Email address
+              </label>
+              <div className="mt-2">
+                <input
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  type="email"
+                  name="email"
+                  id="email"
+                  required
+                  className="block w-full rounded-md bg-gray px-3 py-1.5 text-base text-white-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                />
+              </div>
+            </div>
+
+            <div>
+              <div className="flex items-center justify-between">
+                <label className="block text-sm/6 font-medium text-white-900">
+                  Password
+                </label>
+                <div className="text-sm">
+                  <a
+                    href="#"
+                    className="font-semibold text-indigo-600 hover:text-indigo-500"
+                  >
+                    Forgot password?
+                  </a>
+                </div>
+              </div>
+              <div className="mt-2">
+                <input
+                  type="password"
+                  name="password"
+                  id="password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  required
+                  className="block w-full rounded-md bg-gray px-3 py-1.5 text-base text-white-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                />
+              </div>
+            </div>
+
+            <div>
+              <button
+                type="submit"
+                className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              >
+                Sign in
+              </button>
+              <p>{message}</p>
+            </div>
+          </form>
+
+          <p className="mt-10 text-center text-sm/6 text-gray-500">
+            Not a member?
+            <Link to="/Register"> Register</Link>
+          </p>
+        </div>
+      </div>
     </div>
   );
 }

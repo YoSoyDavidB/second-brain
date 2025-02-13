af645d5ec7bb24d356195c1c3bbd228250e7263d
David Buitrago <david.buitrgo@qualitasfunds.com>
2025-02-13 19:21:34 +0100

    Refactor Dockerfile and Vite config
    - Update Dockerfile to use Node 22-alpine, install dependencies, and expose port 3000
    - Modify Vite config to set base path, configure plugins, and define server settings

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss()
-  ],
-})
+  base: "/",
+  plugins: [react(), tailwindcss()],
+  preview: {
+    port: 8080,
+    strictPort: true,
+  },
+  server: {
+    port: 8080,
+    strictPort: true,
+    host: true,
+    origin: "http://0.0.0.0:8080",
+  },
+});
